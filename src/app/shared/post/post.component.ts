bc25042a7dd888cd912e9c01844c64a34bffe514
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-12-17 13:00:43 +0100

    adds post component

--- src/app/shared/post/post.component.ts
+++ src/app/shared/post/post.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  selector: "app-post",
+  templateUrl: "./post.component.html",
+  styleUrls: ["./post.component.css"],
 })
 export class PostComponent implements OnInit {
+  open: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSeeMoreClick() {
+    this.open = !this.open;
   }
-
 }
