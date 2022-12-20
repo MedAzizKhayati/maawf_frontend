53da7ec660029ea1e60def5710f86eae33cc00ce
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-12-20 18:31:57 +0100

    adds modal logic

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from "@angular/core";
+import { async } from "rxjs";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styles: [
-  ]
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styles: [],
 })
 export class ModalComponent implements OnInit {
+  @Input()
+  template: TemplateRef<any> | null;
+  @Input()
+  onSubmit: () => Promise<any> | any;
+  @Input()
+  onClose: () => Promise<any> | any;
+  @Input()
+  title: string = "Title";
+  @Input()
+  isDisabled: boolean = true;
+  @Input()
+  isVisible: boolean = false;
 
-  constructor() { }
+  isLoading: boolean = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  async submit() {
+    this.isLoading = true;
+    try {
+      await this.onSubmit();
+      await this.close();
+    } catch (e) {
+      console.error(e);
+    }
   }
 
+  async close() {
+    try {
+      await this.onClose();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      this.isVisible = false;
+    }
+  }
 }
