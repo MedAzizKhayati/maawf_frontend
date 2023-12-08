3e99957193855b978b0cac02feac51c1952a18d0
Mohamed Aziz Khayati <mohamed.aziz.khayati@divizend.com>
2023-12-08 01:21:08 +0100

    feat: added environment tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { environment } from "@/environments/environment.prod";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html"
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  constructor(
-  ) { }
+  environment?: string;
+  constructor() {
+    if (environment.apiURL.includes("develop")) {
+      this.environment = "develop";
+    } else if (environment.apiURL.includes("test")) {
+      this.environment = "test";
+    }
+  }
 }
